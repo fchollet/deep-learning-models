def38584fdd3154ab09c26e33ee45961f99eb504
Kashif Rasul <kashif.rasul@gmail.com>
2016-10-03 23:04:17 +0200

    finished dense hypercolumn layer

--- autocolorize.py
+++ autocolorize.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 import numpy as np
 import warnings
 
-from keras.layers import merge, Input, Dropout, Reshape, Deconvolution2D
+from keras.layers import merge, Input, Dropout, Reshape, Deconvolution2D, TimeDistributed
 from keras.layers import Dense, Activation, Flatten
 from keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D, AveragePooling2D
 from keras.layers import BatchNormalization
@@ -15,8 +15,9 @@ from keras.utils.data_utils import get_file
 from imagenet_utils import decode_predictions, preprocess_input
 import keras.utils.visualize_util as vutil
 
+
 def AutoColorize(include_top=True, weights='imagenet',
-                input_tensor=None):
+                 input_tensor=None):
     if weights not in {'imagenet', None}:
         raise ValueError('The `weights` argument should be either '
                          '`None` (random initialization) or `imagenet` '
@@ -60,7 +61,6 @@ def AutoColorize(include_top=True, weights='imagenet',
 
     # Block 4
     conv4_1 = Convolution2D(512, 3, 3, activation='relu', border_mode='same', name='conv4_1')(pool3)
-    print(conv4_1._keras_shape)
     conv4_2 = Convolution2D(512, 3, 3, activation='relu', border_mode='same', name='conv4_2')(conv4_1)
     conv4_3 = Convolution2D(512, 3, 3, activation='relu', border_mode='same', name='conv4_3')(conv4_2)
     pool4 = MaxPooling2D((2, 2), strides=(2, 2), name='pool4')(conv4_3)
@@ -78,8 +78,7 @@ def AutoColorize(include_top=True, weights='imagenet',
     # Block 7
     fc7 = Convolution2D(4096, 1, 1, activation='relu', border_mode='same', name='fc7')(dropout_fc6)
     dropout_fc7 = Dropout(0.5, name='dropout_fc7')(fc7)
-    #fc7_reshaped = Reshape((4096,16,16), name='fc7_reshaped')(dropout_fc7)
-    # fc7_full_reshaped = Deconvolution2D(4096, 16, 16, (1, 4096, 16, 16), subsample=(8,8), border_mode='same', name='fc7_full_reshaped')(fc7_reshaped)
+    print(dropout_fc7._keras_shape)
 
     # Dense Hypercolumn
     data_full = AveragePooling2D(pool_size=(4, 4), strides=(4, 4), name='data_full')(img_input)
@@ -88,12 +87,59 @@ def AutoColorize(include_top=True, weights='imagenet',
     conv2_1_full = AveragePooling2D(pool_size=(2, 2), strides=(2, 2), name='conv2_1_full')(conv2_1)
     conv2_2_full = AveragePooling2D(pool_size=(2, 2), strides=(2, 2), name='conv2_2_full')(conv2_2)
 
-    conv4_1_reshaped = Reshape((-1, 1))(conv4_1)
-    print(conv4_1_reshaped._keras_shape)
+    conv4_1_reshaped = Reshape((512, 1, 64, 64), name='conv4_1_reshaped')(conv4_1)
+    conv4_1_full_reshaped = TimeDistributed(
+        Deconvolution2D(1, 4, 4, subsample=(2, 2), output_shape=(None, 1, 128, 128), border_mode='same'),
+        input_shape=(None, 512, 1, 64, 64), name='conv4_1_full_reshaped')(conv4_1_reshaped)
+    conv4_1_full = Reshape((512, 128, 128), name='conv4_1_full')(conv4_1_full_reshaped)
 
+    conv4_2_reshaped = Reshape((512, 1, 64, 64), name='conv4_2_reshaped')(conv4_2)
+    conv4_2_full_reshaped = TimeDistributed(
+        Deconvolution2D(1, 4, 4, subsample=(2, 2), output_shape=(None, 1, 128, 128), border_mode='same'),
+        input_shape=(None, 512, 1, 64, 64), name='conv4_2_full_reshaped')(conv4_2_reshaped)
+    conv4_2_full = Reshape((512, 128, 128), name='conv4_2_full')(conv4_2_full_reshaped)
 
-    dense_hypercolumn = merge([conv3_3, conv3_2, conv3_1, conv2_2_full, conv2_1_full, conv1_2_full, conv1_1_full, data_full], mode='concat',
-                              name='dense_hypercolumn', concat_axis=1)
+    conv4_3_reshaped = Reshape((512, 1, 64, 64), name='conv4_3_reshaped')(conv4_3)
+    conv4_3_full_reshaped = TimeDistributed(
+        Deconvolution2D(1, 4, 4, subsample=(2, 2), output_shape=(None, 1, 128, 128), border_mode='same'),
+        input_shape=(None, 512, 1, 64, 64), name='conv4_3_full_reshaped')(conv4_3_reshaped)
+    conv4_3_full = Reshape((512, 128, 128), name='conv4_3_full')(conv4_3_full_reshaped)
+
+    conv5_1_reshaped = Reshape((512, 1, 32, 32), name='conv5_1_reshaped')(conv5_1)
+    conv5_1_full_reshaped = TimeDistributed(
+        Deconvolution2D(1, 8, 8, subsample=(4, 4), output_shape=(None, 1, 128, 128), border_mode='same'),
+        input_shape=(None, 512, 1, 32, 32), name='conv5_1_full_reshaped')(conv5_1_reshaped)
+    conv5_1_full = Reshape((512, 128, 128), name='conv5_1_full')(conv5_1_full_reshaped)
+
+    conv5_2_reshaped = Reshape((512, 1, 32, 32), name='conv5_2_reshaped')(conv5_2)
+    conv5_2_full_reshaped = TimeDistributed(
+        Deconvolution2D(1, 8, 8, subsample=(4, 4), output_shape=(None, 1, 128, 128), border_mode='same'),
+        input_shape=(None, 512, 1, 32, 32), name='conv5_2_full_reshaped')(conv5_2_reshaped)
+    conv5_2_full = Reshape((512, 128, 128), name='conv5_2_full')(conv5_2_full_reshaped)
+
+    conv5_3_reshaped = Reshape((512, 1, 32, 32), name='conv5_3_reshaped')(conv5_3)
+    conv5_3_full_reshaped = TimeDistributed(
+        Deconvolution2D(1, 8, 8, subsample=(4, 4), output_shape=(None, 1, 128, 128), border_mode='same'),
+        input_shape=(None, 512, 1, 32, 32), name='conv5_3_full_reshaped')(conv5_3_reshaped)
+    conv5_3_full = Reshape((512, 128, 128), name='conv5_3_full')(conv5_3_full_reshaped)
+
+    fc6_reshaped = Reshape((4096, 1, 16, 16), name='fc6_reshaped')(dropout_fc6)
+    fc6_full_reshaped = TimeDistributed(
+        Deconvolution2D(1, 16, 16, subsample=(8, 8), output_shape=(None, 1, 128, 128), border_mode='same'),
+        input_shape=(None, 4096, 1, 16, 16), name='fc6_full_reshaped')(fc6_reshaped)
+    fc6_full = Reshape((4096, 128, 128), name='fc6_full')(fc6_full_reshaped)
+
+    fc7_reshaped = Reshape((4096, 1, 16, 16), name='fc7_reshaped')(dropout_fc7)
+    fc7_full_reshaped = TimeDistributed(
+        Deconvolution2D(1, 16, 16, subsample=(8, 8), output_shape=(None, 1, 128, 128), border_mode='same'),
+        input_shape=(None, 4096, 1, 16, 16), name='fc7_full_reshaped')(fc7_reshaped)
+    fc7_full = Reshape((4096, 128, 128), name='fc7_full')(fc7_full_reshaped)
+
+    dense_hypercolumn = merge(
+        [fc7_full, fc6_full, conv5_3_full, conv5_2_full, conv5_1_full, conv4_3_full, conv4_2_full, conv4_1_full,
+         conv3_3, conv3_2, conv3_1, conv2_2_full, conv2_1_full,
+         conv1_2_full, conv1_1_full, data_full], mode='concat',
+        name='dense_hypercolumn', concat_axis=1)
 
     # Fully connected
     h_fc1 = Convolution2D(1024, 1, 1, activation='relu', border_mode='same', name='h_fc1')(dense_hypercolumn)
