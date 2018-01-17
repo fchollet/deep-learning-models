6e829a7b44496bd08814d461858af9f86afc8d52
Anon George <anongeorge007@gmail.com>
2018-01-17 16:29:06 +0530

    include_top changed to require_flatten
    include_top=include_top does not work in new keras version and had to be changed to require_flatten= include_top

--- vgg16.py
+++ vgg16.py
@@ -99,7 +99,7 @@ def VGG16(include_top=True, weights='imagenet',
                                       default_size=224,
                                       min_size=48,
                                       data_format=K.image_data_format(),
-                                      include_top=include_top)
+                                      require_flatten=include_top)
 
     if input_tensor is None:
         img_input = Input(shape=input_shape)
